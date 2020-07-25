f46ddfd3ab0e2ad9e62acf1eb6a474b4c02b25b7
Wilson Aceros <wilson.acerosc@gmail.com>
2020-07-25 17:25:06 -0500

    Ejercicios python basico hechos el 25 Julio  2020

--- conversor2.py
+++ conversor2.py
@@ -1,10 +1,38 @@
-"""Convertir de pesos a dolares, pesos mexicanos
-o pesos argentinos"""
+menu="""
+Bienvenido al conversor de Monedas 💲 a dolares EEUU
 
-pesos=input("Cuantos pesos colombianos tienes?: ")
-pesos=float(pesos)
-valor_dolar=3875
-dolares=pesos/valor_dolar
-dolares=round(dolares,2)
-dolares=str(dolares)
-print(f'Tienes ${dolares} dolares')
+1 - Pesos Colombianos
+2 - Pesos Mexicanos
+3 - Dolar Canadiense
+
+Elige una opcion: """
+
+opcion=int(input(menu))
+
+if opcion==1:
+    pesos=input("Cuantos pesos colombianos tienes?: ")
+    pesos=float(pesos)
+    valor_dolar=3875
+    dolares=pesos/valor_dolar
+    dolares=round(dolares,2)
+    dolares=str(dolares)
+    print(f'Tienes ${dolares} dolares americanos')
+elif opcion ==2:
+    pesos=input("Cuantos pesos mexicanos tienes?: ")
+    pesos=float(pesos)
+    valor_dolar=24
+    dolares=pesos/valor_dolar
+    dolares=round(dolares,2)
+    dolares=str(dolares)
+    print(f'Tienes ${dolares} dolares americanos')
+elif opcion==3:
+    pesos=input("Cuantos dolares canadienses tienes?: ")
+    pesos=float(pesos)
+    valor_dolar=1.34
+    dolares=pesos/valor_dolar
+    dolares=round(dolares,2)
+    dolares=str(dolares)
+    print(f'Tienes ${dolares} dolares americanos')
+else:
+    print("Elige una opcion correcta!")
+
