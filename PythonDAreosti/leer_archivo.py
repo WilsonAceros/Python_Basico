ae66eef2c689c9271e3c6216d07acd54845a886f
Wilson Aceros <wilson.acerosc@gmail.com>
2020-08-23 00:27:00 -0500

    Ejercicios 23 Agosto curso Python DAreosti

--- PythonDAreosti/leer_archivo.py
+++ PythonDAreosti/leer_archivo.py
@@ -2,7 +2,7 @@ def run():
     counter = 0
     with open('cuento.txt',encoding='utf-8') as f:
         #print(f.readlines())
-        
+
         for line in f:
             counter += line.count('Beatriz')
 
