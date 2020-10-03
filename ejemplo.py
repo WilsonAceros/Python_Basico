08d2fe5fc2b384b09eeaefc79341f46b6967b70a
Wilson Aceros <wilson.acerosc@gmail.com>
2020-10-02 19:42:25 -0500

    Archivos organizados pensamiento computacional

--- ejemplo.py
+++
@@ -1,9 +0,0 @@
-n=int(input('Un entero: '))
-m=int(input('Otro entero: '))
-
-if n>m:
-    print('El primer numero es mayor')
-elif n<m:
-    print('El segundo numero es mayor')
-else:
-    print('Los dos numeros son iguales')
